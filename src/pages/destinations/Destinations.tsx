43e5f05845237f5036ac3caf65ec9b82d04a6424
anacatarinafg <anafrontenddeveloper@gmail.com>
2023-07-23 20:07:48 +0100

    view more button

--- src/pages/destinations/Destinations.tsx
+++ src/pages/destinations/Destinations.tsx
@@ -5,13 +5,30 @@ import { Link } from "react-router-dom";
 
 const Destinations = () => {
   const [search, setSearch] = useState("");
+  const [load, setLoad] = useState(6);
+
+  const showMore = () => {
+    setLoad((previousValue) => previousValue + 3);
+  };
 
   // console.log(destinations.filter((destination) => destination.place.toLowerCase().includes("du")));
 
   return (
     <div className="destinations">
       <div className="destinations__top">
-        <h2 className="destinations__headline">All our destinations</h2>
+        <div className="top__left">
+          <h2 className="destinations__headline">Destinations</h2>
+          <p className="destinations__text">
+            Experience the incredible beauty of Scandinavia, the Nordic and
+            Baltic countries, scenic landscapes of Ireland and historic sights
+            of the United Kingdom. Experience the incredible beauty of Scandinavia, the Nordic and Baltic countries, scenic landscapes of Ireland and historic sights of the United Kingdom.
+          </p>
+        </div>
+        <div className="top__right">
+          <img src="./assets/worldgif.gif" alt="world-map"></img>
+        </div>
+      </div>
+      <div className="destinations__searchBox">
         <input
           type="search"
           placeholder="Type to search..."
@@ -24,6 +41,7 @@ const Destinations = () => {
           .filter((destination) =>
             destination.place.toLowerCase().includes(search.toLocaleLowerCase())
           )
+          .slice(0, load)
           .map((destination) => (
             <Link
               to={`/destinations/${destination.id}`}
@@ -31,12 +49,16 @@ const Destinations = () => {
               key={destination.id}
             >
               <img src={destination.img[0]} alt={destination.place} />
-              <div className="destination__info">
-                <h2 className="destination__place">{destination.place}</h2>
-                <p className="destination__text">{destination.title}</p>
+              <div className="destinations__info">
+                <h2 className="destinations__place">{destination.place}</h2>
               </div>
             </Link>
           ))}
+      </div>
+      <div className="destinations__buttonBox">
+        <button onClick={showMore} className="destinations__button">
+          More destinations
+        </button>
       </div>
       {/* REMINDER TO MAKE A FILTER */}
     </div>
