d44c54f4aebeb77e1fd32c4ed00895648d1477a2
anacatarinafg <anafrontenddeveloper@gmail.com>
2023-07-26 16:31:47 +0100

    bg change & button selected color

--- src/pages/destinations/Destinations.tsx
+++ src/pages/destinations/Destinations.tsx
@@ -7,6 +7,7 @@ const Destinations = () => {
   const [search, setSearch] = useState("");
   const [load, setLoad] = useState(12);
   const [filter, setFilter] = useState(destinations);
+  const [activeButtonId, setActiveButtonId] = useState("all");
 
   const maxPrice = destinations.reduce((max, destination) => {
     return destination.price > max ? destination.price : max;
@@ -54,91 +55,162 @@ const Destinations = () => {
     setFilter(filteredByPrice);
   };
 
+  // Function to clear all the filters selected
+  const clearFilters = () => {};
+
+
+  // Define a type for the button IDs
+  type ButtonId =
+    | "all"
+    | "africa"
+    | "america"
+    | "asia"
+    | "antarctica"
+    | "australia"
+    | "europe";
+
+  // Function to handle button click and set the active button ID
+  const handleButtonClick = (buttonId: ButtonId) => {
+    setActiveButtonId(buttonId);
+  };
+
   return (
-    <div className="destinations">
-      <div className="destinations__top">
-        <div className="top__left">
-          <h2 className="destinations__headline">Destinations</h2>
-          <p className="destinations__text">
-            Here at <i>viajar</i> we set about to equip the savvy traveller find
-            all they need to know to plan their next trip and provide them with
-            better booking options. With our mix of locals' knowledge, select
-            destination partners and the latest web-technology, we keep right
-            up-to-date with all the latest news, weather, events and offers...
-            our detailed destination guides mean you'll never be short of things
-            to see, places to stay, or unique experiences to try.
-          </p>
+    <div className="destinations__bg">
+      <div className="destinations">
+        <div className="destinations__top">
+          <div className="top__left">
+            <h2 className="destinations__headline">Destinations</h2>
+            <img src="./assets/worldgif.gif" alt="world-map"></img>
+          </div>
         </div>
-        <div className="top__right">
-          <img src="./assets/worldgif.gif" alt="world-map"></img>
+        <div className="destinations__filter">
+          <h4>Where do you want to go:</h4>
+          <div>
+            <button
+              onClick={() => {
+                setFilter(destinations);
+                handleButtonClick("all");
+              }}
+              className={activeButtonId === "all" ? "active" : ""}
+            >
+              All
+            </button>
+            <button
+              onClick={() => {
+                filterDestinations("Africa");
+                handleButtonClick("africa");
+              }}
+              className={activeButtonId === "africa" ? "active" : ""}
+            >
+              Africa
+            </button>
+            <button
+              onClick={() => {
+                filterDestinations("America");
+                handleButtonClick("america");
+              }}
+              className={activeButtonId === "america" ? "active" : ""}
+            >
+              America
+            </button>
+            <button
+              onClick={() => {
+                filterDestinations("Asia");
+                handleButtonClick("asia");
+              }}
+              className={activeButtonId === "asia" ? "active" : ""}
+            >
+              Asia
+            </button>
+            <button
+              onClick={() => {
+                filterDestinations("Antartica");
+                handleButtonClick("antarctica");
+              }}
+              className={activeButtonId === "antarctica" ? "active" : ""}
+            >
+              Antarctica
+            </button>
+            <button
+              onClick={() => {
+                filterDestinations("Australia");
+                handleButtonClick("australia");
+              }}
+              className={activeButtonId === "australia" ? "active" : ""}
+            >
+              Australia
+            </button>
+            <button
+              onClick={() => {
+                filterDestinations("Europe");
+                handleButtonClick("europe");
+              }}
+              className={activeButtonId === "europe" ? "active" : ""}
+            >
+              Europe
+            </button>
+          </div>
+          <div className="destinations__priceFilter">
+            <h4>Price range between:</h4>
+            <input
+              type="range"
+              name="range"
+              min={minPrice}
+              max={maxPrice}
+              value={price}
+              onChange={(e) => {
+                setPrice(parseFloat(e.target.value));
+                filterPrice(e.target.value);
+              }}
+            ></input>
+            <h4>{price}€</h4>
+          </div>
         </div>
-      </div>
-      <div className="destinations__filter">
-        <h4>Where do you want to go:</h4>
-        <div>
-          <button onClick={() => setFilter(destinations)}>All</button>
-          <button onClick={() => filterDestinations("Africa")}>Africa</button>
-          <button onClick={() => filterDestinations("America")}>America</button>
-          <button onClick={() => filterDestinations("Asia")}>Asia</button>
-          <button onClick={() => filterDestinations("Antarctica")}>
-            Antarctica
-          </button>
-          <button onClick={() => filterDestinations("Australia")}>
-            Australia
-          </button>
-          <button onClick={() => filterDestinations("Europe")}>Europe</button>
-        </div>
-        <div className="destinations__priceFilter">
-          <h4>Cheaper to higher places:</h4>
+        <div className="destinations__searchBox">
           <input
-            type="range"
-            name="range"
-            min={minPrice}
-            max={maxPrice}
-            value={price}
-            onChange={(e) => {
-              setPrice(parseFloat(e.target.value));
-              filterPrice(e.target.value);
-            }}
+            type="search"
+            placeholder="Type to search..."
+            className="destinations__search"
+            onChange={(e) => setSearch(e.target.value)}
           ></input>
-          <h4>{price}€</h4>
+          <button
+            onChange={() => clearFilters()}
+            onClick={() => setFilter(destinations)}
+            className="destinations__clearFilters"
+          >
+            Clear filters
+          </button>
         </div>
-      </div>
-      <div className="destinations__searchBox">
-        <input
-          type="search"
-          placeholder="Type to search..."
-          className="destinations__search"
-          onChange={(e) => setSearch(e.target.value)}
-        ></input>
-      </div>
-      <div className="destinations__wrapper">
-        {filter.length > 0 ? (
-          filter
-            .filter((destination) =>
-              destination.place.toLowerCase().includes(search.toLowerCase())
-            )
-            .slice(0, load)
-            .map((destination) => (
-              <Link
-                to={`/destinations/${destination.id}`}
-                className="destinations__box"
-                key={destination.id}
-              >
-                <img src={destination.img[0]} alt={destination.place} />
-                <div className="destinations__info">
-                  <h2 className="destinations__place">{destination.place}</h2>
-                </div>
-              </Link>
-            ))
-        ) : (
-          <p className="destinations__error">No destinations matching the selected continent.</p>
-        )}
-      </div>
-      <div className="destinations__buttonBox">
-        <button onClick={showMore} className="destinations__button">
-          More destinations
-        </button>
+        <div className="destinations__wrapper">
+          {filter.length > 0 ? (
+            filter
+              .filter((destination) =>
+                destination.place.toLowerCase().includes(search.toLowerCase())
+              )
+              .slice(0, load)
+              .map((destination) => (
+                <Link
+                  to={`/destinations/${destination.id}`}
+                  className="destinations__box"
+                  key={destination.id}
+                >
+                  <img src={destination.img[0]} alt={destination.place} />
+                  <div className="destinations__info">
+                    <h2 className="destinations__place">{destination.place}</h2>
+                  </div>
+                </Link>
+              ))
+          ) : (
+            <p className="destinations__error">
+              No destinations matching the selected continent.
+            </p>
+          )}
+        </div>
+        <div className="destinations__buttonBox">
+          <button onClick={showMore} className="destinations__button">
+            More destinations
+          </button>
+        </div>
       </div>
     </div>
   );
